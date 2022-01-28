c4940578997145383b7cb2014379604e78008dce
Oleg Shilov <oleg.shilov@nodamatics.com>
2022-01-29 00:28:52 +0300

    chore: add @polkadot/api to deps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ApiPromise, WsProvider } from '@polkadot/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-polkadot-test';
